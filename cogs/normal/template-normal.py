4f9cc13c99db9e52ccefcdac35eea165ad6520b6
c10ud <66269103+C10udCantDev@users.noreply.github.com>
2022-03-21 16:54:43 -0400

    Add files via upload

---
+++ cogs/normal/template-normal.py
@@ -0,0 +1,67 @@
+""""
+Copyright © Krypton 2021 - https://github.com/kkrypt0nn (https://krypt0n.co.uk)
+Description:
+This is a template to create your own discord bot in python.
+
+Version: 4.1
+"""
+
+import json
+import os
+import sys
+
+from disnake.ext import commands
+from disnake.ext.commands import Context
+
+from helpers import checks
+
+# Only if you want to use variables that are in the config.json file.
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+
+
+# Here we name the cog and create a new class for the cog.
+class Template(commands.Cog, name="template-normal"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(
+        name="testcommand",
+        description="This is a testing command that does nothing.",
+    )
+    @checks.not_blacklisted()
+    @checks.is_owner()
+    async def testcommand(self, context: Context) -> None:
+        """
+        This is a testing command that does nothing.
+        :param context: The context in which the command has been executed.
+        """
+        # Do your stuff here
+
+        # Don't forget to remove "pass", that's just because there's no content in the method.
+        pass
+
+
+# And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
+def setup(bot):
+    bot.add_cog(Template(bot))
+
+
+
+
+
+
+
+
+    @client.command(pass_context=True)
+async def meme(ctx):
+    embed = discord.Embed(title="", description="")
+
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
